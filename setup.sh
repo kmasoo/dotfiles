2c631489125b5ab5b8b96b292748da14e10c69ff
masoo <masoo@hafx>
2018-09-04 08:35:38 +0900

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -8,28 +8,15 @@ do
     fi
 done
 
-USER=masoo
 DOTFILES=${HOME}/prj/dotfiles/.emacs.d/lisp
 
-echo ${USER}
-echo $DOTFILES
-
-# cd ~${USER}/prj/dotfiles
-if [ -e $DOTFILES ];
-then
-  cd $DOTFILES
-else
-    echo 'No anything directory...'
-    exit
-fi
-
-echo $DOTFILES
+cd $DOTFILES
 
 if [ ! -e $DOTFILES/anything/anything-config.el ];
 then
     git clone https://github.com/emacsattic/anything
 else
-    echo 'Existing the anything files...  exit setup script.'
+    echo 'The anything files are Existing ...  exit setup script.'
 fi
 
 
