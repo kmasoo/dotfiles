d3a807d6ff29bc82880aa3dd6eb95a5fc6a4c2fb
masoo <masoo@hafx>
2018-09-04 07:59:20 +0900

    1.delete "don't use directory & files" , 2.available "git clone anything"

--- setup.sh
+++ setup.sh
@@ -8,3 +8,28 @@ do
     fi
 done
 
+USER=masoo
+DOTFILES=${HOME}/prj/dotfiles/.emacs.d/lisp
+
+echo ${USER}
+echo $DOTFILES
+
+# cd ~${USER}/prj/dotfiles
+if [ -e $DOTFILES ];
+then
+  cd $DOTFILES
+else
+    echo 'No anything directory...'
+    exit
+fi
+
+echo $DOTFILES
+
+if [ ! -e $DOTFILES/anything/anything-config.el ];
+then
+    git clone https://github.com/emacsattic/anything
+else
+    echo 'Existing the anything files...  exit setup script.'
+fi
+
+
