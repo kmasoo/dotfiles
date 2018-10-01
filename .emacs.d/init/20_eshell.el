28d22c5d8dfb1d2a80d8c9f9c4ab30b7940b1cb4
masoo <masoo@hafx>
2018-10-01 23:58:02 +0900

    rm --cached

--- .emacs.d/init/20_eshell.el
+++
@@ -1,8 +0,0 @@
-
-
-;; load environment value
-(load-file (expand-file-name "~/.emacs.d/config/shellenv.el"))
-(dolist (path (reverse (split-string (getenv "PATH") ":")))
-  (add-to-list 'exec-path path))
-
-
--- .emacs.d/init/20_multi_term.el
+++
@@ -1,5 +0,0 @@
-;; multi-term setting
-;; (when (require 'multi-term nil t)
-;;   ;; used shell
-;;   (setq multi-term-program "/usr/bin/zsh")
-;; )
