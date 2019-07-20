d54efd25bcb3f4904facc32422cbe1e7497bd252
masoo <masoo@x250>
2019-07-20 22:22:44 +0900

    change require -> use-package

--- .emacs.d/init/20_ivy.el
+++ .emacs.d/init/20_ivy.el
@@ -2,43 +2,91 @@
 ;;; Ivy setting
 ;;----------------------------------------------------------------------
 
+;;-------------------------------------
 ;; ivy設定
-(require 'ivy)
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq ivy-height 30) ;; minibufferのサイズを拡大！（重要）
-(setq ivy-extra-directories nil)
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-plus)))
+;;-------------------------------------
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (setq ivy-height 30) ;; minibufferのサイズを拡大！（重要）
+  (setq ivy-extra-directories nil)
+  (setq ivy-re-builders-alist
+	'((t . ivy--regex-plus)))
+  )
+
 (setq counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))
 
+;;-------------------------------------
+;; counsel
+;;-------------------------------------
+(use-package counsel
+  :ensure t
+  :config
+  )
+;;-------------------------------------
+;; counsel-etags
+;;-------------------------------------
+(use-package counsel-etags
+  :ensure t
+  :config
+  )
+;;-------------------------------------
+;; counsel-gtags
+;;-------------------------------------
+(use-package counsel-gtags
+  :ensure t
+  :config
+  )
+;;-------------------------------------
+;; all-the-icons-ivy
+;;-------------------------------------
+(use-package all-the-icons-ivy
+  :ensure t
+  :config
+  (all-the-icons-ivy-setup)
+)
 
-(all-the-icons-ivy-setup)
-(require 'all-the-icons-ivy)
 (ivy-set-display-transformer 'ivy-switch-buffer 'all-the-icons-ivy-buffer-transformer)
-
- ;;swiper
-
+;;-------------------------------------
+;;swiper
+;;-------------------------------------
+(use-package swiper
+  :ensure t
+  :config
+  )
 ;;(global-set-key "\C-s" 'swiper)
 (setq swiper-include-line-number-in-search t) ;; line-numberでも検索可能
 
-;; migemo + swiper（日本語をローマ字検索できるようになる）
-;;(require 'avy-migemo)
-;;(avy-migemo-mode 1)
+;; migemo + swiper（日本語をローマ字検索できるようになる
+;;-------------------------------------
+;; avy-migemo
+;;-------------------------------------
+(use-package avy-migemo
+  :ensure t
+  :config
+  (avy-migemo-mode 1)
 ;;(require 'avy-migemo-e.g.swiper)
+)
 
-
+;;-------------------------------------
 ;; company
+;;-------------------------------------
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode) ; 全バッファで有効にする
+  (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+  (setq company-idle-delay 0) ; デフォルトは0.5
+  (setq company-minimum-prefix-length 3) ; デフォルトは4
+  (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+  (setq completion-ignore-case t)
+  (setq company-dabbrev-downcase nil)
+  (company-mode t) ;; 自動補完
+)
 
-(require 'company)
-(global-company-mode) ; 全バッファで有効にする
-(setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
-(setq company-idle-delay 0) ; デフォルトは0.5
-(setq company-minimum-prefix-length 3) ; デフォルトは4
-(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
-(setq completion-ignore-case t)
-(setq company-dabbrev-downcase nil)
 (global-set-key (kbd "C-M-i") 'company-complete)
 (define-key company-active-map (kbd "C-n") 'company-select-next) ;; C-n, C-pで補完候補を次/前の候補を選択
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
@@ -49,7 +97,21 @@
 (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
 (define-key company-active-map (kbd "C-f") 'company-complete-selection) ;; C-fで候補を設定
 (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete) ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
+
 ;; yasnippetとの連携
+;;-------------------------------------
+;; yasnippet
+;;-------------------------------------
+(use-package yasnippet
+  :ensure t
+  :config
+  )
+;;(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(setq yas-snippet-dirs
+    '("~/.emacs.d/yasnippets" ;; お好みで!!
+    "~/.emacs.d/mysnippets"))
+(yas-global-mode 1)
+
 (defvar company-mode/enable-yas t
   "Enable yasnippet for all backends.")
 (defun company-mode/backend-with-yas (backend)
@@ -59,15 +121,17 @@
             '(:with company-yasnippet))))
 (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
 
-
-
+;;-------------------------------------
+;; git-complete
+;;-------------------------------------
 ;; git-complete
 (require 'git-complete)
 
 
 
+;;-------------------------------------
 ;; recentf
-
+;;-------------------------------------
 ;; 余分なメッセージを削除しておきましょう
 (defmacro with-suppressed-message (&rest body)
   "Suppress new messages temporarily in the echo area and the `*Messages*' buffer while BODY is evaluated."
@@ -75,43 +139,59 @@
   (let ((message-log-max nil))
     `(with-temp-message (or (current-message) "") ,@body)))
 
-(require 'recentf)
-(setq recentf-save-file "~/.emacs.d/recentf")
-(setq recentf-max-saved-items 200)             ;; recentf に保存するファイルの数
-(setq recentf-exclude '("recentf"))           ;; .recentf自体は含まない
-(setq recentf-auto-cleanup 'never)             ;; 保存する内容を整理
-(run-with-idle-timer 30 t '(lambda () (with-suppressed-message (recentf-save-list))))
-(require 'recentf-ext) ;; ちょっとした拡張
+(use-package recentf
+  :ensure t
+  :config
+  (setq recentf-save-file "~/.emacs.d/recentf")
+  (setq recentf-max-saved-items 200)             ;; recentf に保存するファイルの数
+  (setq recentf-exclude '("recentf"))           ;; .recentf自体は含まない
+  (setq recentf-auto-cleanup 'never)             ;; 保存する内容を整理
+  (run-with-idle-timer 30 t '(lambda () (with-suppressed-message (recentf-save-list))))
+)
 
+;;(require 'recentf-ext) ;; ちょっとした拡張
+;;-------------------------------------
+;; recentf-ext
+;;-------------------------------------
+(use-package recentf-ext
+  :ensure t
+  :config
+  )
+
+;;-------------------------------------
 ;; neotree
+;;-------------------------------------
+(use-package neotree
+  :ensure t
+  :config
+  (setq neo-theme 'ascii) ;; icon, classic等もあるよ！
+  (setq neo-persist-show t) ;; delete-other-window で neotree ウィンドウを消さない
+  (setq neo-smart-open t) ;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
+  (setq neo-smart-open t)
+  ;;(global-set-key "\C-o" 'neotree-toggle)
+)
 
-(setq neo-theme 'ascii) ;; icon, classic等もあるよ！
-(setq neo-persist-show t) ;; delete-other-window で neotree ウィンドウを消さない
-(setq neo-smart-open t) ;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
-(setq neo-smart-open t)
-(global-set-key "\C-o" 'neotree-toggle)
-
-;; yasnippet
-(require 'yasnippet)
-;;(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
-(setq yas-snippet-dirs
-    '("~/.emacs.d/yasnippets" ;; お好みで!!
-    "~/.emacs.d/mysnippets"))
-(yas-global-mode 1)
-
-(setq dumb-jump-mode t)
-(setq dumb-jump-selector 'ivy) ;; 候補選択をivyに任せます
-(setq dumb-jump-use-visible-window nil)
+;;-------------------------------------
+;; dumb-jump
+;;-------------------------------------
+(use-package dumb-jump
+  :ensure t
+  :config
+  (setq dumb-jump-mode t)
+  (setq dumb-jump-selector 'ivy) ;; 候補選択をivyに任せます
+  (setq dumb-jump-use-visible-window nil)
+)
 ;;(define-key global-map [(super d)] 'dumb-jump-go) ;; go-to-definition!
 ;;(define-key global-map [(super shift d)] 'dumb-jump-back)
 
-
-
-(company-mode t) ;; 自動補完
-
-;; ivy-rich settings
-(require 'ivy-rich)
-(ivy-rich-mode 1)
+;;-------------------------------------
+;; ivy-rich
+;;-------------------------------------
+(use-package ivy-rich
+  :ensure t
+  :config
+  (ivy-rich-mode 1)
+)
 
 (defun ivy-rich-switch-buffer-icon (candidate)
      (with-current-buffer
@@ -137,14 +217,13 @@
 
 
 
-;;(global-set-key (kbd "M-x") 'counsel) ;; コマンド補完
 (global-set-key (kbd "C-s") 'swiper) ;; buffer内検索
 (global-set-key (kbd "C-c C-c") 'git-complete) ;; gitによる補完
 ;;(global-set-key (kbd "super-shift-i") 'find-file-in-project) ;; ファイル検索
 (global-set-key (kbd "C-x C-r") 'counsel-recentf) ;; 履歴によるファイル検索
 ;;(global-set-key (kbd "super-f") 'counsel-rg) ;; find-usage
 (global-set-key (kbd "C-c C-d") 'dumb-jump) ;; go-to-definition
-(global-set-key (kbd "C-o") 'neotree) ;; ディレクトリツリー展開
+(global-set-key (kbd "C-x t") 'neotree) ;; ディレクトリツリー展開
 
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
@@ -153,7 +232,7 @@
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
 (global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;;(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 
 ;; これらは counsel-mode で自動で設定されるため、明示的に設定しなくてよい
 ;;(global-set-key (kbd "M-x") 'counsel-M-x)
